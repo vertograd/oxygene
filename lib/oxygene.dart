bd77dcf0bfab2a55e917e3870c71ac8f39748001
Ivan Rybets <mitopipe@Mac-mini-Ivan.local>
2024-06-20 00:30:07 +0300

    first commit

---
+++ lib/oxygene.dart
@@ -0,0 +1,12 @@
+library oxygene;
+
+import 'package:flutter/material.dart';
+
+class Oxygene extends StatelessWidget {
+  const Oxygene({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+}
---
+++ test/oxygene_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:oxygene/oxygene.dart';
+
+void main() {
+  test('adds one to input values', () {
+    const Oxygene();
+  });
+}
